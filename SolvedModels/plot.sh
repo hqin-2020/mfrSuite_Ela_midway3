66f0abcca1c4122f8aa9db4f1552c8452b86c816
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-15 16:58:55 -0600

    1

--- SolvedModels/plot.sh
+++ SolvedModels/plot.sh
@@ -35,12 +35,12 @@ do
                             mkdir -p ./job-outs/$domain_folder/$model_folder
                             mkdir -p ./bash/$domain_folder/$model_folder
 
-                            touch ./bash/$domain_folder/$model_folder/run.sh
-                            tee ./bash/$domain_folder/$model_folder/run.sh << EOF
+                            touch ./bash/$domain_folder/$model_folder/plot.sh
+                            tee ./bash/$domain_folder/$model_folder/plot.sh << EOF
 #! /bin/bash
 
 #SBATCH --account=pi-lhansen
-#SBATCH --job-name=run
+#SBATCH --job-name=plot
 #SBATCH --output=./job-outs/$domain_folder/$model_folder/plot.out
 #SBATCH --error=./job-outs/$domain_folder/$model_folder/plot.err
 #SBATCH --time=0-10:00:00
@@ -51,11 +51,11 @@ do
 
 module load python/anaconda-2021.05
 
-python3 /project/lhansen/mfrSuite_Ela_midway3/SolvedModels/plots.py --chiUnderline ${chiUnderline} --a_e ${a_e} --a_h ${a_h} --gamma_e ${gamma_e} --gamma_h ${gamma_h} --psi_e ${psi_e} --psi_h ${psi_h} \
+python3 /project/lhansen/mfrSuite_Ela_midway3/SolvedModels/plot.py --chiUnderline ${chiUnderline} --a_e ${a_e} --a_h ${a_h} --gamma_e ${gamma_e} --gamma_h ${gamma_h} --psi_e ${psi_e} --psi_h ${psi_h} \
                                                     --nV ${nV} --nVtilde ${nVtilde} --V_bar ${V_bar} --Vtilde_bar ${Vtilde_bar} --sigma_V_norm ${sigma_V_norm} --sigma_Vtilde_norm ${sigma_Vtilde_norm} \
 
 EOF
-                            sbatch ./bash/$domain_folder/$model_folder/run.sh
+                            sbatch ./bash/$domain_folder/$model_folder/plot.sh
                         done
                     done
                 done
