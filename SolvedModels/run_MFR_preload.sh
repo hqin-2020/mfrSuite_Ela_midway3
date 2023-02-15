3ad4f0f8af67380d3601954868494aa787610832
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-15 17:05:58 -0600

    1

--- SolvedModels/run_MFR_preload.sh
+++ SolvedModels/run_MFR_preload.sh
@@ -35,14 +35,14 @@ do
                             mkdir -p ./job-outs/$domain_folder/$model_folder
                             mkdir -p ./bash/$domain_folder/$model_folder
 
-                            touch ./bash/$domain_folder/$model_folder/run.sh
-                            tee ./bash/$domain_folder/$model_folder/run.sh << EOF
+                            touch ./bash/$domain_folder/$model_folder/pre.sh
+                            tee ./bash/$domain_folder/$model_folder/pre.sh << EOF
 #! /bin/bash
 
 #SBATCH --account=pi-lhansen
-#SBATCH --job-name=run
-#SBATCH --output=./job-outs/$domain_folder/$model_folder/run.out
-#SBATCH --error=./job-outs/$domain_folder/$model_folder/run.err
+#SBATCH --job-name=pre
+#SBATCH --output=./job-outs/$domain_folder/$model_folder/pre.out
+#SBATCH --error=./job-outs/$domain_folder/$model_folder/pre.err
 #SBATCH --time=0-10:00:00
 #SBATCH --partition=caslake
 #SBATCH --nodes=1
@@ -55,7 +55,7 @@ python3 /project/lhansen/mfrSuite_Ela_midway3/SolvedModels/preload.py --chiUnder
                                                     --nV ${nV} --nVtilde ${nVtilde} --V_bar ${V_bar} --Vtilde_bar ${Vtilde_bar} --sigma_V_norm ${sigma_V_norm} --sigma_Vtilde_norm ${sigma_Vtilde_norm} \
 
 EOF
-                            sbatch ./bash/$domain_folder/$model_folder/run.sh
+                            sbatch ./bash/$domain_folder/$model_folder/pre.sh
                         done
                     done
                 done
